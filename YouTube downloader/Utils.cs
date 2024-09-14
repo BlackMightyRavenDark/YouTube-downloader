43bfed8bb6807367db1f090cbdf2a4360dae0272
BlackRain <masaraksh86@mail.ru>
2024-09-14 17:16:30 +0700

    Исправлен порядок using'ов в классе "Utils"

--- YouTube downloader/Utils.cs
+++ YouTube downloader/Utils.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
