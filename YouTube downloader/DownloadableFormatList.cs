450947e0933c3c4b8bb1bca8c7f1a4378cd78148
BlackRain <masaraksh86@mail.ru>
2026-06-16 14:24:46 +0700

    Исправлен метод "DownloadableFormatList.SplitFormats()"

--- YouTube downloader/DownloadableFormatList.cs
+++ YouTube downloader/DownloadableFormatList.cs
@@ -54,11 +54,10 @@ namespace YouTube_downloader
 			UrlsExpirationDate = video.UrlsValidUntil;
 		}
 
-		private void SplitFormats()
+		private bool SplitFormats()
 		{
 			if (!_isFormatsSplitted)
 			{
-				_isFormatsSplitted = true;
 				if (_isYtdlVideo)
 				{
 					YtdlVideo v = _video as YtdlVideo;
@@ -66,6 +65,7 @@ namespace YouTube_downloader
 					AudioOnlyTracks = FilterAudioTracks(v.TrackList).ToList();
 					Containers = FilterContainerTracks(v.TrackList).ToList();
 					Streams = FilterHlsTracks(v.TrackList).ToList();
+					_isFormatsSplitted = true;
 				}
 				else if (_streamingData != null)
 				{
@@ -74,155 +74,159 @@ namespace YouTube_downloader
 					AudioOnlyTracks = FilterAudioTracks(formatList.Tracks).ToList();
 					Containers = FilterContainerTracks(formatList.Tracks).ToList();
 					Streams = FilterHlsTracks(formatList.Tracks).ToList();
+					_isFormatsSplitted = true;
 				}
 			}
+
+			return _isFormatsSplitted;
 		}
 
 		internal ContextMenuStrip BuildContextMenu(EventHandler menuItemClickHandler)
 		{
-			SplitFormats();
-
-			if (_video.IsDashed)
+			if (SplitFormats())
 			{
-				if (config.SortDashFormatsByBitrate)
+				if (_video.IsDashed)
+				{
+					if (config.SortDashFormatsByBitrate)
+					{
+						int SorterFunc(YouTubeMediaTrack x, YouTubeMediaTrack y)
+						{
+							if ((x.AverageBitrate <= 0 && y.AverageBitrate <= 0) || x.AverageBitrate == y.AverageBitrate)
+							{
+								return 0;
+							}
+							return x.AverageBitrate < y.AverageBitrate ? 1 : -1;
+						}
+
+						VideoOnlyTracks.Sort(SorterFunc);
+						AudioOnlyTracks.Sort(SorterFunc);
+					}
+				}
+				else if (config.SortFormatsByFileSize)
 				{
 					int SorterFunc(YouTubeMediaTrack x, YouTubeMediaTrack y)
 					{
-						if ((x.AverageBitrate <= 0 && y.AverageBitrate <= 0) || x.AverageBitrate == y.AverageBitrate)
+						if ((x.ContentLength <= 0L && y.ContentLength <= 0L) || x.ContentLength == y.ContentLength)
 						{
 							return 0;
 						}
-						return x.AverageBitrate < y.AverageBitrate ? 1 : -1;
+						return x.ContentLength < y.ContentLength ? 1 : -1;
 					}
 
 					VideoOnlyTracks.Sort(SorterFunc);
 					AudioOnlyTracks.Sort(SorterFunc);
 				}
-			}
-			else if (config.SortFormatsByFileSize)
-			{
-				int SorterFunc(YouTubeMediaTrack x, YouTubeMediaTrack y)
-				{
-					if ((x.ContentLength <= 0L && y.ContentLength <= 0L) || x.ContentLength == y.ContentLength)
-					{
-						return 0;
-					}
-					return x.ContentLength < y.ContentLength ? 1 : -1;
-				}
 
-				VideoOnlyTracks.Sort(SorterFunc);
-				AudioOnlyTracks.Sort(SorterFunc);
-			}
-
-			if (Streams.Count > 1)
-			{
-				Streams.Sort((x, y) =>
+				if (Streams.Count > 1)
 				{
-					if (x.VideoHeight > 0 || y.VideoHeight > 0)
+					Streams.Sort((x, y) =>
 					{
-						if (x.VideoHeight == y.VideoHeight)
+						if (x.VideoHeight > 0 || y.VideoHeight > 0)
 						{
-							return x.AverageBitrate < y.AverageBitrate ? 1 : -1;
+							if (x.VideoHeight == y.VideoHeight)
+							{
+								return x.AverageBitrate < y.AverageBitrate ? 1 : -1;
+							}
+
+							return x.VideoHeight < y.VideoHeight ? 1 : -1;
 						}
 
-						return x.VideoHeight < y.VideoHeight ? 1 : -1;
-					}
+						if (x.AverageBitrate == y.AverageBitrate) { return 0; }
+						return x.AverageBitrate < y.AverageBitrate ? 1 : -1;
+					});
+				}
 
-					if (x.AverageBitrate == y.AverageBitrate) { return 0; }
-					return x.AverageBitrate < y.AverageBitrate ? 1 : -1;
-				});
-			}
-
-			if (config.AlwaysMoveAudioId140ToTopOfList && AudioOnlyTracks.Count > 1)
-			{
-				for (int i = 0; i < AudioOnlyTracks.Count; ++i)
+				if (config.AlwaysMoveAudioId140ToTopOfList && AudioOnlyTracks.Count > 1)
 				{
-					if (AudioOnlyTracks[i].FormatId == 140)
+					for (int i = 0; i < AudioOnlyTracks.Count; ++i)
 					{
-						if (i != 0)
+						if (AudioOnlyTracks[i].FormatId == 140)
 						{
-							(AudioOnlyTracks[i], AudioOnlyTracks[0]) = (AudioOnlyTracks[0], AudioOnlyTracks[i]);
+							if (i != 0)
+							{
+								(AudioOnlyTracks[i], AudioOnlyTracks[0]) = (AudioOnlyTracks[0], AudioOnlyTracks[i]);
+							}
+
+							break;
 						}
-
-						break;
 					}
 				}
-			}
 
-			List<TableRow> tableRows = new List<TableRow>();
-			List<TableColumn> tableColumns = new List<TableColumn>()
-			{
-				new TableColumn(TableColumnAlignment.Left),
-				new TableColumn(TableColumnAlignment.Left),
-				new TableColumn(TableColumnAlignment.Right),
-				new TableColumn(TableColumnAlignment.Right),
-				new TableColumn(TableColumnAlignment.Right),
-				new TableColumn(TableColumnAlignment.Left),
-				new TableColumn(TableColumnAlignment.Left),
-				new TableColumn(TableColumnAlignment.Right),
-				new TableColumn(TableColumnAlignment.Right)
-			};
-
-			bool showHls = Streams.Count > 0 && (!config.ShowHlsTracksOnlyForStreams ||
-				(config.ShowHlsTracksOnlyForStreams && _video.IsLiveNow) || VideoOnlyTracks.Count == 0);
-			if (showHls)
-			{
-				foreach (YouTubeMediaTrackHlsStream trackHls in Streams)
+				List<TableRow> tableRows = new List<TableRow>();
+				List<TableColumn> tableColumns = new List<TableColumn>()
 				{
-					tableRows.Add(trackHls.ToTableRow());
-				}
-			}
+					new TableColumn(TableColumnAlignment.Left),
+					new TableColumn(TableColumnAlignment.Left),
+					new TableColumn(TableColumnAlignment.Right),
+					new TableColumn(TableColumnAlignment.Right),
+					new TableColumn(TableColumnAlignment.Right),
+					new TableColumn(TableColumnAlignment.Left),
+					new TableColumn(TableColumnAlignment.Left),
+					new TableColumn(TableColumnAlignment.Right),
+					new TableColumn(TableColumnAlignment.Right)
+				};
 
-			foreach (YouTubeMediaTrackVideo trackVideo in VideoOnlyTracks)
-			{
-				tableRows.Add(trackVideo.ToTableRow());
-			}
-
-			foreach (YouTubeMediaTrackContainer trackContainer in Containers)
-			{
-				tableRows.Add(trackContainer.ToTableRow());
-			}
-
-			foreach (YouTubeMediaTrackAudio trackAudio in AudioOnlyTracks)
-			{
-				tableRows.Add(trackAudio.ToTableRow());
-			}
-
-			Table table = new Table(tableRows, tableColumns);
-			table.Format();
-			const string columnSeparator = " | ";
-
-			if (table.Rows.Count > 0)
-			{
-				ContextMenuStrip menu = new ContextMenuStrip();
-				menu.Font = new System.Drawing.Font("Lucida Console", config.MenusFontSize);
-				foreach (TableRow row in table.Rows)
+				bool showHls = Streams.Count > 0 && (!config.ShowHlsTracksOnlyForStreams ||
+					(config.ShowHlsTracksOnlyForStreams && _video.IsLiveNow) || VideoOnlyTracks.Count == 0);
+				if (showHls)
 				{
-					string rowText = row.Join(columnSeparator);
-					ToolStripMenuItem mi = new ToolStripMenuItem(rowText)
+					foreach (YouTubeMediaTrackHlsStream trackHls in Streams)
 					{
-						Padding = new Padding(0, 4, 0, 4),
-						Tag = row.Tag
-					};
-					if (menuItemClickHandler != null)
-					{
-						mi.Click += menuItemClickHandler;
+						tableRows.Add(trackHls.ToTableRow());
 					}
-					menu.Items.Add(mi);
 				}
 
-				if (VideoOnlyTracks.Count + AudioOnlyTracks.Count > 0)
+				foreach (YouTubeMediaTrackVideo trackVideo in VideoOnlyTracks)
 				{
-					ToolStripMenuItem mi = new ToolStripMenuItem("Выбрать форматы...") { Tag = null };
-					if (menuItemClickHandler != null)
-					{
-						mi.Click += menuItemClickHandler;
-					}
-					menu.Items.Add(mi);
+					tableRows.Add(trackVideo.ToTableRow());
 				}
 
-				menu.Renderer = new FormatListContextMenuRenderer();
-				return menu;
+				foreach (YouTubeMediaTrackContainer trackContainer in Containers)
+				{
+					tableRows.Add(trackContainer.ToTableRow());
+				}
+
+				foreach (YouTubeMediaTrackAudio trackAudio in AudioOnlyTracks)
+				{
+					tableRows.Add(trackAudio.ToTableRow());
+				}
+
+				Table table = new Table(tableRows, tableColumns);
+				table.Format();
+				const string columnSeparator = " | ";
+
+				if (table.Rows.Count > 0)
+				{
+					ContextMenuStrip menu = new ContextMenuStrip();
+					menu.Font = new System.Drawing.Font("Lucida Console", config.MenusFontSize);
+					foreach (TableRow row in table.Rows)
+					{
+						string rowText = row.Join(columnSeparator);
+						ToolStripMenuItem mi = new ToolStripMenuItem(rowText)
+						{
+							Padding = new Padding(0, 4, 0, 4),
+							Tag = row.Tag
+						};
+						if (menuItemClickHandler != null)
+						{
+							mi.Click += menuItemClickHandler;
+						}
+						menu.Items.Add(mi);
+					}
+
+					if (VideoOnlyTracks.Count + AudioOnlyTracks.Count > 0)
+					{
+						ToolStripMenuItem mi = new ToolStripMenuItem("Выбрать форматы...") { Tag = null };
+						if (menuItemClickHandler != null)
+						{
+							mi.Click += menuItemClickHandler;
+						}
+						menu.Items.Add(mi);
+					}
+
+					menu.Renderer = new FormatListContextMenuRenderer();
+					return menu;
+				}
 			}
 
 			return null;
